df720ee528ee1c4e8e6fad4079eeb807cc7300c6
nw11 <nw11@sanger.ac.uk>
2014-06-16 15:29:20 +0100

    clean up test a bit, still not actually test

--- test/Functions-Test-1.jl
+++ test/Functions-Test-1.jl
@@ -5,33 +5,64 @@ module FunctionTest1
    file1 = Pkg.dir("MethylUtils", "testdata", "moabs-format-1000lines.txt")
    file2 = Pkg.dir("MethylUtils", "testdata", "ensGene1000lines.txt.gz")
 
-  # read test data
+   ##### READ TEST DATA #####
+
+   ## cytosine counts ##
    cytosine_counts         =   load_moabs_format_as_sequence_features(file1)
    println("Cytosine Counts")
    println(cytosine_counts)
+
+   ## annotation file ##
+   ensgene_gene_regions_ft =   make_ensgene_gene_feature_table(file2, "mm10")
+   ensgene_gene_regions    =   FeatureRegions(ensgene_gene_regions_ft)
+
+   ####====####
+
+
+
+
+
+   ###### METHYLATION ESTIMATION FUNCTONS #################
+
    cytosine_methylation    =   run_mc_hmc_calc( cytosine_counts )
 
-   ensgene_gene_regions_df =   make_ensgene_gene_regions(file2, "mm10")
-   ensgene_gene_regions    =   FeatureRegions(ensgene_gene_regions_df)
    println("Cytosines Methylation")
    println(cytosine_methylation)
-   println(ensgene_gene_regions_df)
+   println(ensgene_gene_regions_ft)
 
-  # TILING RELATED
+   ####====####
 
+
+
+
+
+   ###### TILING RELATED  ##############
    by_gene = cytosine_methylation_by_gene( cytosine_methylation, ensgene_gene_regions )
    println(by_gene)
 
-  # UTILS
-  # TEST INTERSECT BED
+   ####====####
+
+
+
+
+   ###### UTILS ############
+
+   # TEST INTERSECT BED
    println("Intersect $file1 and $file2")
    mm10_ensgene_path = Pkg.dir("MethylUtils", "testdata", "ensGene1000lines.bed")
-   feature_table_to_bed(ensgene_gene_regions_df, mm10_ensgene_path )
+   feature_table_to_bed(ensgene_gene_regions_ft, mm10_ensgene_path )
    path = intersect_files(file1, mm10_ensgene_path, overwrite=true)
 
    println("intersected files path: $path")
- # TEST MLML
- # test that we have order cytosines
+
+   ####====####
+
+end
+
+
+
+  # TEST MLML
+  # test that we have order cytosines
 
   # include(Pkg.dir("MethylUtils", "src","FeatureServices.jl"))
   # println("start")
@@ -47,10 +78,6 @@ module FunctionTest1
   # # rm the file for testing
   # SET_LOCAL_STORE_PATH("nothing")
 
-
-
   #rm files for testing next time
   # Pkg.dir("MethylUtils", "testdata", "ensGene1000lines.bed")
   # Pkg.dir("MethylUtils", "testdata", "moabs-format-1000lines.txt.intersect.bed")
-
-end
